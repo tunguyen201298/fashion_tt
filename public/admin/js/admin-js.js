e4f2440d8d96d33a6d8cbb8813be3b08370a5164
tunguyen98 <tudtdt1998@gmail.com>
2020-08-28 16:28:16 +0700

    28/8 tu da ghe

--- public/admin/js/admin-js.js
+++ public/admin/js/admin-js.js
@@ -1,4 +1,5 @@
 
+var check = false;
 
 $(function(){
     $("input[name=phone]")[0].oninvalid = function () {
@@ -6,34 +7,67 @@ $(function(){
     };
 });
 $('#name').on('blur change keyup', function(){
-	var regexname = /^[a-zA-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂẾưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s\W|_]{2,}$/g;
+	var regexname = /^[a-zA-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂẾưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s\W|_]{3,}$/g;
 	var name = $("#name").val();
-	if(name.length == 0){
-		$(".erorrName").text("Tên không được để trông!!!");
-	}else if (!name.match(regexname)) {
+	if (!name.match(regexname)) {
 		$(".erorrName").text("Tên không đúng!!!");
+		$("#name").css('border-color', 'red');
 	}else{
 		$(".erorrName").text(" ");
+		$("#name").css('border-color', 'green');
 	}
 });
-function checkemail(){
+$('#email').on('blur change keyup', function(){
 	const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 	var email = $("#email").val();
-	if(email.length == 0){
-		$(".erorrName").text("Email không được để trông!!!");
-	}else if (!email.match(re)) {
+	if(!email.match(re)) {
 		$(".erorrName").text("Email không đúng!!!");
+		$("#email").css('border-color', 'red');
 	}else{
 		$(".erorrName").text(" ");
+		$("#email").css('border-color', 'green');
 	}
-}
-function checkphone(){
+});
+$('#phone').on('blur change keyup', function(){
 	var phone = $("#phone").val();
-	if (phone.length == 0){
-		$(".erorrName").text("SĐT không được để trông!!!");
-	}else if(phone.length < 10 || phone.length > 15){
-		$(".erorrName").text("SĐT không đúng!!!");
+	if(phone.length < 10 || phone.length > 13){
+		$(".erorrName").text("Số điện thoại không đúng!!!");
+		$("#phone").css('border-color', 'red');
 	}else{
 		$(".erorrName").text(" ");
+		$("#phone").css('border-color', 'green');
 	}
-}
+});
+$('#password').on('blur change keyup', function(){
+	var password = $("#password").val();
+	if(password.length < 6){
+		$(".erorrName").text(" Mật khẩu quá ngắn!!!");
+		$("#phone").css('border-color', 'red');
+	}else if(password.length > 30){
+		$(".erorrName").text("Mật khẩu quá dài!!!");
+		$("#phone").css('border-color', 'red');
+	}else{
+		$(".erorrName").text(" ");
+		$("#phone").css('border-color', 'green');
+	}
+});
+$('#password_confirm').on('blur change keyup', function(){
+	var password_confirm = $("#password_confirm").val();
+	var password = $("#password").val();
+	if(password_confirm != password){
+		$(".erorrName").text("Mật khẩu không khớp!!!");
+		$("#password_confirm").css('border-color', 'red');
+	}else{
+		$(".erorrName").text(" ");
+		$("#password_confirm").css('border-color', 'green');
+	}
+});
+$('#birthday').on('blur change keyup', function(){
+	var birthday = $("#birthday").val();
+	if(birthday.length != 0){
+		$(".erorrName").text(" ");
+		$("#birthday").css('border-color', 'green');
+	}
+});
+
+
---
+++ resources/views/partials/_formValidation.blade.php
@@ -0,0 +1,5 @@
+<!-- Form validation -->
+<link rel="stylesheet" href="{{asset('/public/formValidation/css/formValidation.min.css')}}" />
+<!-- form validation -->
+<script src="{{asset('/public/formValidation/js/formValidation.min.js')}}" > </script>
+<script src="{{asset('/public/formValidation/validate/bootstrap.min.js')}}" > </script>
